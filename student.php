6f2c7c0bad1e9730268a964551c992c1d466305c
leiya rybicki <lmrybick@mtu.edu>
2022-12-04 16:50:22 -0500

    Update student.php
    logout

--- student.php
+++ student.php
@@ -2,3 +2,14 @@
     session_start();
     require "db.php";
 ?>
+<html>
+<form action="login.php" method="post" >
+    <button name="logout">logout</button>
+        <?php
+        if (isset($_POST["logout"])) {
+            header("LOCATION:login.php");
+
+        }
+        ?>
+</form>
+</html>
