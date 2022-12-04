6154a8ded11ee15fc2c6d348dd410b8523baa46e
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-12-04 15:43:07 -0500

    Update login.php

--- login.php
+++ login.php
@@ -12,5 +12,19 @@
 </header>
 
 <body>
-    
+    <div class="login container">
+        <form method="POST" action="login.php">
+            <div class="login-input">
+                <label for="">Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="login-input">
+                <label for="">Password</label>
+                <input type="password" name="password">
+            </div>
+            <div class="login-input">
+                <input type="submit" name="loginSubmit">
+            </div>
+        </form>
+    </div> 
 </body>
