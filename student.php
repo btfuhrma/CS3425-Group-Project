4fbf1362743ac5d6ab196f84cecf33396896a845
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-12-04 16:55:42 -0500

    Update student.php

--- student.php
+++ student.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
     require "db.php";
+    if (!isset($_SESSION["username"])) {
+        header("LOCATION:login.php");
+    }
 ?>
 <html>
 <form action="student.php" method="post" >
