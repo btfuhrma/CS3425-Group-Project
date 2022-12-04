bc9060e658634502868e2b11df5b22c99074ad8d
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-12-04 15:40:01 -0500

    css file

--- login.php
+++ login.php
@@ -5,5 +5,12 @@
 ?>
 
 <header>
-    
+    <link rel="stylesheet" href="style.css">
+    <title>Canvas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
 </header>
+
+<body>
+    
+</body>
