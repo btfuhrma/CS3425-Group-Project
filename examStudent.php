6b19a2f1bd113cee980a4335974d669abe87a421
leiya rybicki <lmrybick@mtu.edu>
2022-12-07 20:14:47 -0500

    Update examStudent.php
    fixes

--- examStudent.php
+++ examStudent.php
@@ -1,57 +1,55 @@
 <html>
 <?php
+    if (isset($_POST["registerCourse"])) {
+
+?>
+
+This is the Register Course page!<br>
+<form action="student.php" method="post">
+<button name="goBack">Go Back</button>
+    <?php
+        if (isset($_POST["goBack"])) {
+            header("LOCATION:student.php");
+        }
+    ?>
+</form>
+
+<?php
+    }
+?>
+
+<?php
+    if (isset($_POST["takeExam"])) {
+
+?>
+
+This is the Take Exam Page!<br>
+Here are the questions for exam (exam name)<br>
+exam questions....<br>
+<form action="student.php" method="post">
+<button name="goBack">Go Back</button>
+    <?php
+        if (isset($_POST["goBack"])) {
+            header("LOCATION:student.php");
+        }
+    ?>
+</form>
+
+<?php
+    }
+?>
+
+<?php
     if (isset($_POST["checkScore"])) {
 
 ?>
+Here is the Check Score Page!<br>
 
-This is the Check Score Page!<br>
-Table for exam info<br>
-Table for student exam info<br>
-<form action="instructor.php" method="post">
-<button name="goBack">Go Back</button>
-    <?php
-        if (isset($_POST["goBack"])) {
-            header("LOCATION:instructor.php");
-        }
-    ?>
-</form>
-
-<?php
-    }
-?>
-
-<?php
-    if (isset($_POST["reviewExam"])) {
-
-?>
-
-This is the Review Exam Page!<br>
-Here are the questions for exam (exam name)<br>
-exam questions....<br>
-<form action="instructor.php" method="post">
-<button name="goBack">Go Back</button>
-    <?php
-        if (isset($_POST["goBack"])) {
-            header("LOCATION:instructor.php");
-        }
-    ?>
-</form>
-
-<?php
-    }
-?>
-
-<?php
-    if (isset($_POST["createExam"])) {
-
-?>
-This function is not implemented yet<br>
-
-<form action="instructor.php" method="post">
+<form action="student.php" method="post">
     <button name="goBack">Go Back</button>
     <?php
         if (isset($_POST["goBack"])) {
-            header("LOCATION:instructor.php");
+            header("LOCATION:student.php");
         }
     ?>
 </form>
