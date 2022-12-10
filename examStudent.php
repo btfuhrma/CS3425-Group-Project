91b72a65bc4b1cb5fda54492babe31f1aab3c778
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-12-10 14:09:07 -0500

    exam for students

--- examStudent.php
+++ examStudent.php
@@ -12,6 +12,25 @@
 <button name="goBack">Go Back</button>
 </form>
 
-
-
+<div class="exam-container">
+    <form>
+        <?php
+            $questions = getExamQuestions($_SESSION["currentExam"]);
+            $i = 0;
+            foreach($questions as $question){
+                $answers = getQuestionAnswers($question);
+                $i++;
+                echo '<div class="question"
+                <p>Q'.$i.' '.$question.'</p>
+                ';
+                foreach($answers as $answer){
+                    echo '
+                    <input type="radio"> 
+                    ';
+                }
+                echo '</div>';
+            }
+        ?>
+    </form>
+</div>
 </html>
